89da7437251ff5382f81bbc7a914811f7b23a267
Allan Lu <48187960+alu2@users.noreply.github.com>
2020-03-27 23:59:05 -0400

    Update covid_scraper.bat

--- covid_scraper.bat
+++ covid_scraper.bat
@@ -1,7 +1,7 @@
-call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
+call "Scripts\activate.bat"
 
 call conda activate ml_env
 
-"C:\ProgramData\Anaconda3\envs\ml_env\python.exe" "C:\Users\allan\Documents\COVID-19\crawler.py"
+"python.exe" "crawler.py"
 
 pause
