6dc9dbb98ec452b7245e18ffc436953b59e56030
Allan Lu <48187960+alu2@users.noreply.github.com>
2020-03-27 23:52:49 -0400

    Add files via upload

---
+++ covid_scraper.bat
@@ -0,0 +1,7 @@
+call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
+
+call conda activate ml_env
+
+"C:\ProgramData\Anaconda3\envs\ml_env\python.exe" "C:\Users\allan\Documents\COVID-19\crawler.py"
+
+pause
